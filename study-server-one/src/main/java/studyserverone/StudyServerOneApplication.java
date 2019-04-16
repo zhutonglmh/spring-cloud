629d95fe870c5b1d26ac254a438b1f690f3620f3
zhutong <zhutonglmh@163.com>
2019-04-16 13:57:15 +0800

    新建学习项目

---
+++ study-server-one/src/main/java/studyserverone/StudyServerOneApplication.java
@@ -0,0 +1,17 @@
+package studyserverone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author 朱同 启动类
+ */
+@SpringBootApplication
+public class StudyServerOneApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(StudyServerOneApplication.class, args);
+    }
+    
+}
